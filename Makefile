1692368498f64a71e8166ca069d31c0e27939689
Martin Petr <git@bodkan.net>
2026-07-22 20:33:17 +0200

    Finalize draft of the GT errors simulation

---
+++ Makefile
@@ -0,0 +1,19 @@
+vcfs := onepop_gt.vcf.gz onepop_gt_bi.vcf.gz onepop_gt_bi_errflat.vcf.gz
+
+all: $(vcfs)
+
+%_gt.vcf.gz: %_sim.R
+	Rscript $<
+
+%_bi.vcf.gz: %.vcf.gz
+	bcftools +fill-tags $< -- -t AF,MAF \
+	  | bcftools view -m2 -M2 \
+	  | bcftools view -e 'COUNT(GT="AA")=N_SAMPLES || COUNT(GT="RR")=N_SAMPLES' -Oz \
+	  > $@
+
+%_bi_errflat.vcf.gz: %_bi.vcf.gz
+	Rscript error_gt.R $< 0.17
+
+clean: 
+	rm -f *.vcf.gz
+
--- error_gt.R
+++ error_gt.R
@@ -1,3 +1,22 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) != 2) {
+  stop("Path to input VCF and error rate [0, 1] must be specified", call. = FALSE)
+}
+
+path <- args[1]
+error_prob <- as.numeric(args[2])
+#path <- "onepop_gt_bi.vcf.gz"
+#error_prob <- 0.14
+
+if (!file.exists(path)) {
+  stop("No file found at the given path", call. = FALSE)
+}
+
+if (!(0 <= error_prob && error_prob <= 1)) {
+  stop("Error probability must be a number between 0 and 1", call. = FALSE)
+}
+
 suppressPackageStartupMessages({
 library(VariantAnnotation)
 library(ggplot2)
@@ -20,19 +39,6 @@ flip_alleles <- function(alleles, error_prob, minor_alleless) {
   alleles
 }
 
-#args <- commandArgs(trailingOnly = TRUE)
-#
-#if (length(args) != 1) {
-#  stop("A single argument specifying the path to a VCF file must be given", call. = FALSE)
-#}
-#
-#path <- args[1]
-path <- "onepop_gt_bi.vcf.gz"
-error_prob <- 0.1
-
-if (!file.exists(path)) {
-  stop("No file found at the given path", call. = FALSE)
-}
 
 info <- Seqinfo(seqnames = "chr1", seqlengths = 1000, isCircular = FALSE, genome = "slendr")
 vcf <- readVcf(path, genome = info)
@@ -45,25 +51,37 @@ vcf <- readVcf(path, genome = info)
 # find out which 0 (REF) or 1 (ALT) allele states at each site are minor alleles
 minor_alleles <- as.character(as.integer(unlist(info(vcf)$AF == info(vcf)$MAF)))
 
+props <- c()
+
 for (s in samples(header(vcf))) {
   cat("Simulating errors in individual", s, "... ")
 
+  # get a vector of (phased) genotypes of this sample and split it into two
+  # vectors with alleles (one for each haplotype)
   gts <- geno(vcf)$GT[, s]
-
   alleles1 <- gsub("\\|.$", "", gts)
   alleles2 <- gsub("^.\\|", "", gts)
 
+  # simulate errors at the given probability
   flipped1 <- flip_alleles(alleles1, error_prob)
   flipped2 <- flip_alleles(alleles2, error_prob)
-
-  mean(alleles1 != flipped1)
-  mean(alleles2 != flipped2)
 
   flipped_gts <- paste(flipped1, flipped2, sep = "|")
 
   geno(vcf)$GT[, s] <- flipped_gts
 
-  cat("done!\n")
+  cat("done! ")
+
+  prop1 <- mean(alleles1[minor_alleles == alleles1] != flipped1[minor_alleles == alleles1])
+  prop2 <- mean(alleles2[minor_alleles == alleles2] != flipped2[minor_alleles == alleles2])
+
+  cat(sprintf("(haplotype 1 errors = %0.2f, haplotype 2 errors = %0.2f\n", prop1, prop2))
+
+  props <- c(props, prop1, prop2)
 }
 
-writeVcf(vcf, "onepop_gt_bi_errflat.vcf.gz")
+cat("-----\nAverage errors across all haplotypes =", mean(props), "\n")
+
+flipped_path <- "onepop_gt_bi_errflat.vcf"
+writeVcf(vcf, flipped_path)
+system(paste("bgzip", flipped_path, ">", paste0(flipped_path, ".gz")))
